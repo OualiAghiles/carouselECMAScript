e5cb59d2e27e6eeb2820de90a548b6e31e47da49
oualiaghiles <ouali.aghiles@gmail.com>
2020-02-23 20:54:39 +0100

    add responsive / infinite / pagination

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "extends": "defaults/configurations/walmart/es6-browser",    
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"]
-    },
-    "env": { "es6": true }
-
-};
--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,35 +1,14 @@
 class slider {
   /**
-   * This callback is displayed as part of the Requester class.
-   * @callback moveCallBack
-   * @param {number} index
-   */
-  /**
-   * @param {HTMLelement} element
-   * @param {object} options
-   * @param {object} options.slidesToScroll *** NBR element to scroll  ***
-   * @param {object} options.slidesVisible *** NBR elements Visible  ***
-   * @param {boolean} options.loop *** doit on bouclé en fin de slide  ***
+
    */
   constructor (element, options = {}) {
     this.element = element
     this.options = Object.assign({}, {
       slidesToScroll : 1,
-      slidesVisible: 1,
-      loop: false
+
     }, options)
-    let children = [].slice.call(element.children)
-    this.currentItem = 0
-    this.moveCallbacks = []
-    this.root = this.createDivWithClass('slider')
-    this.container = this.createDivWithClass('slider__container')
-    this.root.appendChild(this.container)
-    this.element.appendChild(this.root)
-    this.items = children.map((child) => {
-      let item = this.createDivWithClass('slider__item')
-      item.appendChild(child)
-      this.container.appendChild(item)
-      return item
+
     })
     this.setStyle()
     this.createNavigation()
@@ -77,25 +56,9 @@ class slider {
     this.goToItem(this.currentItem - this.options.slidesToScroll)
   }
   /**
-   * goToItem - deplace le slider ver l'element cible
+
    *
-   * @param  {number} index current index
-   * @return {number}       target index
-   */
-  goToItem (index) {
-    if (index < 0) {
-      index = this.items.length - this.options.slidesVisible
-    } else if (index >= this.items.length  || (this.items[this.currentItem + this.options.slidesVisible] === undefined && index > this.currentItem)) {
-      index = 0
-    }
-    this.currentItem = index
-    let translateX = index * (-100 / this.items.length)
-    this.container.style.transform = 'translate3d('+ translateX +'%, 0 , 0)'
-    this.moveCallbacks.forEach(cd => cd(index))
-  }
-  /**
-   *
-   * @param {string} className
+
    * @memberof carousel
    * @returns {HTMLelement}
    */
@@ -104,20 +67,11 @@ class slider {
     div.classList.add(className)
     return div
   }
-  /**
-   *
-   * @param {moveCallBack} cb
-   */
-  onMove (cb) {
-    this.moveCallbacks.push(cb)
-  }
+
 }
 
-document.addEventListener('readystatechange', function () {
+
   new slider(document.querySelector('#slider'), {
-    slidesToScroll: 2,
-    slidesVisible: 2,
-    loop: false
+
   })
 
-})
